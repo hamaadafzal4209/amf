727bb03745ca6ed8656d2d3200aa4a0abc56ff87
FrontendFlair <hamaadafzal404@gmail.com>
2024-11-05 05:53:16 -0800

    commit

--- components/Home/Hero.jsx
+++ components/Home/Hero.jsx
@@ -1,27 +1,32 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
-import Image from 'next/image';
-import { motion, AnimatePresence } from 'framer-motion';
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import { motion, AnimatePresence } from "framer-motion";
 
 const heroContents = [
   {
     title: "Cutting-Edge Electronics",
     description: "Discover the latest in tech innovation",
-    image: "https://plus.unsplash.com/premium_photo-1682145728214-dbd62535af3f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bG93JTIwdm9sdGFnZXxlbnwwfHwwfHx8MA%3D%3D",
-    background: "/assets/banner-1.jpg"
+    image:
+      "https://plus.unsplash.com/premium_photo-1682145728214-dbd62535af3f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bG93JTIwdm9sdGFnZXxlbnwwfHwwfHx8MA%3D%3D",
+    background: "/assets/banner-1.jpg",
   },
   {
     title: "Smart Home Solutions",
     description: "Transform your living space with IoT devices",
-    image: "https://images.unsplash.com/photo-1508841710800-0149d3a22a11?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGxvdyUyMHZvbHRhZ2V8ZW58MHx8MHx8fDA%3D",
-    background: "https://images.unsplash.com/photo-1517373116369-9bdb8cdc9f62?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZWxlY3RyaWMlMjBjaXJjdWl0fGVufDB8fDB8fHww" 
+    image:
+      "https://images.unsplash.com/photo-1508841710800-0149d3a22a11?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGxvdyUyMHZvbHRhZ2V8ZW58MHx8MHx8fDA%3D",
+    background:
+      "https://images.unsplash.com/photo-1517373116369-9bdb8cdc9f62?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZWxlY3RyaWMlMjBjaXJjdWl0fGVufDB8fDB8fHww",
   },
   {
     title: "Powerful Gadgets",
     description: "Boost your productivity with our range of gadgets",
-    image: "https://images.unsplash.com/photo-1490717550892-b6599dd47aa8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGxvdyUyMHZvbHRhZ2V8ZW58MHx8MHx8fDA%3D",
-    background: "https://plus.unsplash.com/premium_photo-1682928136660-8cbeaf4941b2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZWxlY3RyaWMlMjBjaXJjdWl0fGVufDB8fDB8fHww"
+    image:
+      "https://images.unsplash.com/photo-1490717550892-b6599dd47aa8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGxvdyUyMHZvbHRhZ2V8ZW58MHx8MHx8fDA%3D",
+    background:
+      "https://plus.unsplash.com/premium_photo-1682928136660-8cbeaf4941b2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZWxlY3RyaWMlMjBjaXJjdWl0fGVufDB8fDB8fHww",
   },
 ];
 
@@ -42,8 +47,8 @@ export default function HeroSection() {
         className="absolute inset-0"
         style={{
           backgroundImage: `url(${heroContents[currentIndex].background})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
+          backgroundSize: "cover",
+          backgroundPosition: "center",
         }}
         key={currentIndex}
         initial={{ opacity: 0 }}
@@ -77,13 +82,48 @@ export default function HeroSection() {
               </p>
             </motion.div>
           </AnimatePresence>
-          <motion.button
-            className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full text-lg transition duration-300 ease-in-out transform hover:scale-105"
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
+
+          <button
+            href="#_"
+            class="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-main transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
           >
-            Explore Now
-          </motion.button>
+            <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-main group-hover:h-full"></span>
+            <span class="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+              <svg
+                class="w-5 h-5 text-main"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M14 5l7 7m0 0l-7 7m7-7H3"
+                ></path>
+              </svg>
+            </span>
+            <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+              <svg
+                class="w-5 h-5 text-white"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M14 5l7 7m0 0l-7 7m7-7H3"
+                ></path>
+              </svg>
+            </span>
+            <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+              Explore Now
+            </span>
+          </button>
         </motion.div>
         <motion.div
           className="lg:w-1/2 flex justify-center"
