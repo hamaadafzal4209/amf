095964e0b39e2a17a2ce8e07d394d56e5e1efac1
FrontendFlair <hamaadafzal404@gmail.com>
2024-11-28 07:33:11 -0800

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "plus.unsplash.com",
-      "images.unsplash.com",
-      "www.tsca.com.ph",
-      "source.unsplash.com",
-      "www.essmetron.com",
-      "www.amf-sa.com",
-      "cdn.prod.website-files.com",
-      "encrypted-tbn0.gstatic.com",
-      "www.cearsistemi.it",
-      "s.alicdn.com",
-      "localhost",
-      "www.anupamelectricalcontrols.com",
-      "res.cloudinary.com",
-      "example.com",
-      "via.placeholder.com"
+    remotePatterns: [
+      { protocol: "https", hostname: "plus.unsplash.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "www.tsca.com.ph" },
+      { protocol: "https", hostname: "source.unsplash.com" },
+      { protocol: "https", hostname: "www.essmetron.com" },
+      { protocol: "https", hostname: "www.amf-sa.com" },
+      { protocol: "https", hostname: "cdn.prod.website-files.com" },
+      { protocol: "https", hostname: "encrypted-tbn0.gstatic.com" },
+      { protocol: "https", hostname: "www.cearsistemi.it" },
+      { protocol: "https", hostname: "s.alicdn.com" },
+      { protocol: "http", hostname: "localhost" },
+      { protocol: "https", hostname: "www.anupamelectricalcontrols.com" },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "example.com" },
+      { protocol: "https", hostname: "via.placeholder.com" },
     ],
   },
 };
