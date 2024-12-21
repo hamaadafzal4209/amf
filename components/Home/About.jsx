828a9b256b579930e43973fabd72b95e50386bc9
FrontendFlair <hamaadafzal404@gmail.com>
2024-12-21 03:08:09 -0800

    commit

--- components/Home/About.jsx
+++ components/Home/About.jsx
@@ -13,11 +13,11 @@ const ABOUT_LIST = [
 
 export default function AboutUs() {
   return (
-    <div className="relative">
-      <DotPattern className="absolute inset-0 z-0 fill-neutral-400/80" />
+  <div className="">
+      <DotPattern className="absolute inset-0 opacity-40 z-0" />
 
       {/* Content Area */}
-      <div className="relative z-10 bg-gray-100">
+      <div className="relative z-10 bg-main">
         <div className="container mx-auto px-4 md:px-8 lg:px-12 pt-16 pb-4">
           <Fade triggerOnce direction="up" duration={800} cascade damping={0.2}>
             <div className="flex flex-col md:flex-row items-center md:items-stretch justify-between gap-12">
@@ -35,16 +35,16 @@ export default function AboutUs() {
 
               <div className="flex-1 flex flex-col justify-center">
                 <Image
-                  src={"/assets/energy.png"}
+                  src={"/assets/whiteEnergy.png"}
                   alt="Bullet"
                   width={1000}
                   height={1000}
                   className="w-6 mb-3 object-contain"
                 />
-                <h1 className="text-4xl font-semibold mb-4 text-main">
+                <h1 className="text-4xl font-semibold mb-4 text-white">
                   About Almaram Alfaneyah ManufacturingCo.
                 </h1>
-                <p className="max-w-3xl text-base text-gray-400 text-balance leading-relaxed">
+                <p className="max-w-3xl text-base text-gray-100 text-balance leading-relaxed">
                   We specialize in designing and manufacturing high-quality
                   electrical panel boards tailored to meet diverse industrial
                   and commercial needs. Our commitment to precision, safety, and
@@ -54,8 +54,8 @@ export default function AboutUs() {
                 <ul className="list-none space-y-2 mt-4">
                   {ABOUT_LIST.map((item, index) => (
                     <li key={index} className="flex items-center gap-3">
-                      <CheckCircle className="text-main" />
-                      <p className="text-sm text-gray-600 leading-relaxed">
+                      <CheckCircle className="text-white" />
+                      <p className="text-sm text-gray-50 leading-relaxed">
                         {item}
                       </p>
                     </li>
@@ -68,18 +68,18 @@ export default function AboutUs() {
           <Fade triggerOnce direction="up" duration={800} cascade damping={0.2}>
             <section className="flex flex-col md:flex-row items-center justify-between gap-12 mt-16 pb-8">
               <div className="flex-1">
-                <Disc2 size={32} className="text-main mb-4" />
-                <h2 className="text-3xl font-semibold mb-6 text-main">
+                <Disc2 size={32} className="text-white mb-4" />
+                <h2 className="text-3xl font-semibold mb-6 text-white">
                   Our Vision
                 </h2>
-                <p className="max-w-2xl mx-auto text-lg text-muted-foreground leading-relaxed">
+                <p className="max-w-2xl mx-auto text-lg text-gray-100 leading-relaxed">
                   To remain committed to our customers by providing complete
                   solutions with an innovative approach, utilizing the latest
                   technology, and building a skilled workforce dedicated to
                   exploring and advancing in the field of power distribution.
                 </p>
               </div>
-              <div className="flex-1 w-full">
+              <div className="flex-1">
                 <figure className="imageSHineEffect">
                   <Image
                     src="/assets/about-2.jpg"
