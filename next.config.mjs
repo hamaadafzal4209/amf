493d5a60493b0546d49d985ada1aa85b6a95ddca
FrontendFlair <hamaadafzal404@gmail.com>
2025-01-12 03:56:04 -0800

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "plus.unsplash.com" },
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "www.tsca.com.ph" },
-      { protocol: "https", hostname: "source.unsplash.com" },
-      { protocol: "https", hostname: "www.essmetron.com" },
-      { protocol: "https", hostname: "www.amf-sa.com" },
-      { protocol: "https", hostname: "cdn.prod.website-files.com" },
-      { protocol: "https", hostname: "encrypted-tbn0.gstatic.com" },
-      { protocol: "https", hostname: "www.cearsistemi.it" },
-      { protocol: "https", hostname: "s.alicdn.com" },
-      { protocol: "http", hostname: "localhost" },
-      { protocol: "https", hostname: "www.anupamelectricalcontrols.com" },
-      { protocol: "https", hostname: "res.cloudinary.com" },
-      { protocol: "https", hostname: "example.com" },
-      { protocol: "https", hostname: "via.placeholder.com" },
-    ],
+    remotePatterns: [{ protocol: "https", hostname: "res.cloudinary.com" }],
   },
 };
 
