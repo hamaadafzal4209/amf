e46b5549866ea0724f3ac490460e8633cc3f0c6d
FrontendFlair <hamaadafzal404@gmail.com>
2025-01-12 22:11:43 -0800

    commit

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -38,7 +38,6 @@ export default async function handler(req, res) {
   const mailOptions = {
     from: `AMF <${process.env.SMTP_MAIL}>`,
     to: "info@amf-sa.com",
-    // to: "hamaadafzal516@gmail.com",
     subject: `Message from ${name}`,
     html: htmlContent,
   };
